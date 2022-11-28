e51f4e0ddf1c40c90c3f01aadf9064858569e61f
James <james.jcarline@gmail.com>
2022-11-28 14:49:06 +0000

    Add psuedo code to stock-picker function

--- spec/stock-picker-spec.rb
+++ spec/stock-picker-spec.rb
@@ -4,7 +4,7 @@ require_relative '../stock-picker'
 RSpec.describe 'Stock Picker' do
     describe 'will return' do
         it 'optimal buying/selling days when given an array' do
-            expect(stock_picker([3,4,5,1,5,8,19,13,12])).to eq([1,19])
+            expect(stock_picker([3,4,5,1,5,8,19,13,12])).to eq([3,6])
         end
         it 'an error when argument is not an array' do
             expect(stock_picker('This is a test')).to eq('Type Error. Please provide an array as an argument.')
--- stock-picker.rb
+++ stock-picker.rb
@@ -1,5 +1,11 @@
 require 'pry-byebug'
 
 def stock_picker(prices)
-    
+    if prices.is_a?
+    # find the max price in the array
+    # find the smallest price in the array that has a smaller index number than the largest.
+    # return an array containing the index values of those two numbers.
+   else
+    return 'Type Error. Please provide an array as an argument.'
+   end
 end
