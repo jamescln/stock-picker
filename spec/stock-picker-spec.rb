a5afe80d885d5f674d45521ec8adb9774e33fc2e
James <james.jcarline@gmail.com>
2022-11-28 14:06:00 +0000

    Update rspec files with basic tests

---
+++ spec/stock-picker-spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require_relative '../stock-picker'
+
+RSpec.describe 'Stock Picker' do
+    describe 'will return' do
+        it 'optimal buying/selling days when given an array' do
+            expect(stock_picker([3,4,5,1,5,8,19,13,12])).to eq([1,19])
+        end
+        it 'an error when argument is not an array' do
+            expect(stock_picker('This is a test')).to eq('Type Error. Please provide an array as an argument.')
+        end
+    end
+end
