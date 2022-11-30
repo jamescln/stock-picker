c8dc5caa900bcffabecf8f3b5f8f445494de9c20
James <james.jcarline@gmail.com>
2022-11-30 18:11:42 +0000

    Complete function with correct dates returned

--- stock-picker.rb
+++ stock-picker.rb
@@ -4,11 +4,11 @@ def stock_picker(prices)
     if prices.kind_of?(Array)
     prices_array = prices
     max_price = prices_array.max
-    binding.pry
     possible_buy_dates = prices[0..prices.index(max_price)]
-    binding.pry
     min_price = possible_buy_dates.min
-    buy_sell_dates = [min_price,max_price]
+    max_price_index = prices.index(max_price)
+    min_price_index = prices.index(min_price)
+    buy_sell_dates = [min_price_index,max_price_index]
     return buy_sell_dates    # find the smallest price in the array that has a smaller index number than the largest.
     # return an array containing the index values of those two numbers.
    else
@@ -17,4 +17,3 @@ def stock_picker(prices)
 end
 
 best_dates = stock_picker([3,4,5,2,5,8,1,4,9,1,4,5])
-binding.pry
