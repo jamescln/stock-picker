7254d9b08e8fa815229f75602f9bdd35f4a5f7b4
James <james.jcarline@gmail.com>
2022-11-30 17:05:45 +0000

    Begin to structure the array methods

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,11 +1,20 @@
 require 'pry-byebug'
 
 def stock_picker(prices)
-    if prices.is_a?
-    # find the max price in the array
-    # find the smallest price in the array that has a smaller index number than the largest.
+    if prices.kind_of?(Array)
+    prices_array = prices
+    max_price = prices_array.max
+    binding.pry
+    possible_buy_dates = prices[0..prices.index(max_price)]
+    binding.pry
+    min_price = possible_buy_dates.min
+    buy_sell_dates = [min_price,max_price]
+    return buy_sell_dates    # find the smallest price in the array that has a smaller index number than the largest.
     # return an array containing the index values of those two numbers.
    else
     return 'Type Error. Please provide an array as an argument.'
    end
 end
+
+best_dates = stock_picker([3,4,5,2,5,8,1,4,9,1,4,5])
+binding.pry
