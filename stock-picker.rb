6dc06563501c7cfb7d71784b6f55b0e2369318a8
James <james.jcarline@gmail.com>
2022-12-01 17:06:27 +0000

    Create nested array containing prices and indeces

--- stock-picker.rb
+++ stock-picker.rb
@@ -2,15 +2,19 @@ require 'pry-byebug'
 
 def stock_picker(prices)
     if prices.kind_of?(Array)
-    permutations = prices.permutation(2).to_a
+        price_with_index = []
+        prices.each_with_index do |price, index|
+            price_with_index << [price,index]
+        end    
+        binding.pry
+    #permutations = prices.permutation(2).to_a
     #permutations.each_with_index do | row, row_index|    
     #end
-    permutation_differences = []
-    permutations.each_with_index do |element, i|
-        permutation_differences[i] = (permutations[i][1] - permutations[i][0])
-    end
-    max_difference = permutation_differences.max
-    binding.pry
+    #permutation_differences = []
+    #permutations.each_with_index do |element, i|
+    #    permutation_differences[i] = (permutations[i][1] - permutations[i][0])
+    #end
+    #max_difference = permutation_differences.max
     max_price = prices.max
     possible_buy_dates = prices[0..prices.index(max_price)]
     min_price = possible_buy_dates.min
